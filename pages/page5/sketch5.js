f649cfd44bf48dd4c6aa013f02b4c6b055d78580
MonCadavreExquis <98280487+MonCadavreExquis@users.noreply.github.com>
2022-04-21 14:28:08 -0400

    Add files via upload

--- pages/page5/sketch5.js
+++ pages/page5/sketch5.js
@@ -41,7 +41,7 @@ p.draw = function(){
  
   graph.fill(0);
   graph.rect(0,0,1300,930);
-  let d=600;
+  let d=800;
  let  r = 255;
   let b = 0;
   for(let i=0; i<50; i++){
@@ -65,12 +65,12 @@ if(rendu === 1){
  p.text('Ce soir-là, j’essaie de m’endormir, mais je n’ai pas de moutons. ', 20, 142);
  p.text('Je compte les craques, dans le plafond.', 20, 170);
  p.text('Un insecte d’échappe d’une crevasse.', 20, 198);
- p.text('Son corps est petit, rond. ', 20, 226);
- p.text('Le mouvement est furtif ; les pattes, rapides. ', 20, 254);
- p.text('Il s’arrête, un instant. Me regarde.', 20, 310);
+ p.text('Son corps est petit et rond. ', 20, 226);
+ p.text('Le mouvement est furtif , les pattes sont rapides. ', 20, 254);
+ p.text('L’insecte s’arrête, un instant. Me regarde.', 20, 310);
  p.text('Est-ce qu’il se reconnaît, dans mon regard vacant? ', 20, 338);
  p.text('Lorsqu’il retourne dans son trou, il m’emmène avec lui.', 20, 366);
- p.text('Je suis aspirée. Dans le sommeil, dans le plafond, je ne suis pas certaine.', 20, 394);
+ p.text('Je suis aspirée. Dans le sommeil, dans le plafond, je ne sais pas.', 20, 394);
  p.text('Tout à coup, je suis dans un nid. Un terrier. ', 20, 478);
  p.text('Il fait chaud, humide, terreux. Devant moi : une masse fourmillante.', 20, 506);
  p.text('Des insectes se collent entre eux. Ils ont ma taille. Certains, plus grands.', 20, 534);
@@ -84,23 +84,39 @@ if(rendu === 1){
 }
 
 if (rendu === 2){
-  p.text('Je n’ai pas peur de la folie, mais il faut me montrer comment faire.', 20, 30);
+  p.background(0);
+   graph.fill(0);
+  graph.rect(0,0,1300,930);
+  let d=800;
+ let  r = 255;
+  let b = 0;
+  for(let i=0; i<50; i++){
+    graph.erase(2*i,0);
+      graph.ellipse(p.mouseX,p.mouseY,d,d);
+     
+      d-=10;
+      r-= 10;
+      b+=10;
+      
+      graph.noErase();
+  }
+  p.text('Je n’ai pas peur de la folie, mais il faut me montrer comment ça marche.', 20, 30);
  p.text('« Tu es une des nôtres, tu sssssssais? »', 20, 86);
  p.text('Il s’approche de moi, me grimpe dessus. ', 20, 142);
  p.text('Je suis couverte de ses pattes cartilagineuses, pendant un moment.', 20, 170);
- p.text('Puis il s’installe à mes côtés. ', 20, 198);
+ p.text('Puis il s’installe à mon côté. ', 20, 198);
  p.text('« Pas encore complètement, mais bientôt... »', 20, 254);
  p.text('Je suis confortable, c’est étrange. Comme hypnotisée par la voix bruissante, la tiédeur environnante.', 20, 310);
  p.text('Le cliquetis des créatures qui nous entourent.', 20, 338);
  p.text('Le roi insecte place sa bouche près de mon cou.', 20, 366);
  p.text('Son souffle est moite. Ses mandibules bougent.', 20, 394);
- p.text('« Quand les gens rêvent de nature, tu sais, ils ne rêvent jamais à nous.', 20, 450);
+ p.text('« Quand les gens rêvent de nature, tu sais, ils ne rêvent jamais de nous.', 20, 450);
  p.text('Ils dizzzent notre nom avec dédain. » ', 20, 478);
-  p.text('Les autres insectes s’approchent de nous. Comme des petits, demandant le réconfort d’un câlin.', 20, 534);
+  p.text('Les autres insectes s’approchent de nous. Des petits, demandant le réconfort d’un câlin.', 20, 534);
  p.text('« Les gens nous casent dans leurs cauchemars, font des monssssstres de nous. »', 20, 590);
  p.text('Les enfants-insectes nous couvrent de leurs corps minéraux.', 20, 646);
  p.text('« Nous ssssssommes incontrôlables : c’est pour ça que les humains nous détesssstent. »', 20, 702);
- p.text('Je me sens chez nous, au milieu de cet amas ondoyant.', 20, 758);
+ p.text('Je me sens chez nous, au creux de cet amas ondoyant.', 20, 758);
  p.text('Les créatures bougent, s’ajustent autour de moi.', 20, 786);
  p.text('Elles aussi, écoutent le récit.', 20, 814);
 
@@ -110,6 +126,22 @@ if (rendu === 2){
 }
 
 if (rendu === 3){
+   p.background(0);
+   graph.fill(0);
+  graph.rect(0,0,1300,930);
+  let d=800;
+ let  r = 255;
+  let b = 0;
+  for(let i=0; i<50; i++){
+    graph.erase(2*i,0);
+      graph.ellipse(p.mouseX,p.mouseY,d,d);
+     
+      d-=10;
+      r-= 10;
+      b+=10;
+      
+      graph.noErase();
+  }
   p.text('« Sssssais-tu comment faire, pour différencier un insecte d’un cheveu,', 20, 30);
  p.text('quand il y a un frisssssson sur la peau? »', 20, 58);
  p.text('J’ai un frisson, sur ma peau. Une caresse froide et délicate.', 20, 114);
@@ -118,13 +150,13 @@ if (rendu === 3){
  p.text('« Noooon. Regarder, c’est la pire chose à faire.', 20, 254);
  p.text('Les insectes, on peut ssssse transformer en cheveux, quand on nous regarde. »', 20, 282);
  p.text('Une mandibule effleure ma nuque.', 20, 338);
- p.text('« Sssssais-tu comment faire, pour différencier un insecte d’un bouton', 20, 394);
+ p.text('« Sssssais-tu comment faire, pour différencier un insecte d’un bouton,', 20, 394);
   p.text('quand quelque chose grouille sssssous la peau? »', 20, 422);
  p.text('Quelque chose grouille alors, sous la peau de mon bras. Je ne regarde pas.', 20, 478);
   p.text('« On peut toucher? » je réponds.', 20, 534);
  p.text('« Noooon. Toucher, c’est la pire chose à faire. ', 20, 590);
-  p.text('Les insectes, on peut avoir peur, quand on nous touche.', 20, 618);
- p.text('Alors, on se cache sous la peau, ssssans bouger. On fait sssssemblant d’être une partie de toi. »', 20, 646);
+  p.text('Les insectes, on a peur, quand on nous touche.', 20, 618);
+ p.text('Alors, on se cache sous la peau, ssssans bouger. On fait sssssemblant d’être une partie de vous. »', 20, 646);
  p.text('Sous la peau de mon bras, quelque chose s’installe, et ne bouge pas.', 20, 702);
  p.text('« Quand un insecte sssse convainc qu’il fait partie de toi, c’est presque impossssible de le faire partir.', 20, 758);
  p.text('Il est bien là. Il fait chaud, humide, confortable, dans un humain. ', 20, 786);
@@ -137,11 +169,27 @@ if(p.millis()>(time+5000)){
 }
 
 if (rendu === 4){
+   p.background(0);
+   graph.fill(0);
+  graph.rect(0,0,1300,930);
+  let d=800;
+ let  r = 255;
+  let b = 0;
+  for(let i=0; i<50; i++){
+    graph.erase(2*i,0);
+      graph.ellipse(p.mouseX,p.mouseY,d,d);
+     
+      d-=10;
+      r-= 10;
+      b+=10;
+      
+      graph.noErase();
+  }
   p.text('Oui.... Je les sens. Je les sens me grignoter.', 20, 30);
-  p.text('« Ssssais-tu comment tu peux savoir qu’un insecte dans ton corps est bien un insecte, ', 20, 86);
+  p.text('« Ssssais-tu comment tu peux savoir, qu’un insecte dans ton corps est bien un insecte, ', 20, 86);
  p.text('et pas autre chose?»', 20, 114);
- p.text('Autour de nous : les corps, les antennes s’agitent avec de plus en plus d’enthousiasme.', 20, 170);
- p.text('Des pattes graffignent ici et là ma chair infestée.', 20, 198);
+ p.text('Autour de nous : les corps, les antennes, s’agitent avec de plus en plus d’enthousiasme.', 20, 170);
+ p.text('Ici et là, des pattes graffignent ma chair infestée.', 20, 198);
  p.text('« Un inssssecte, ça va exactement où tu ne veux pas que ça aille.', 20, 254);
  p.text('Et peu importe combien tu esssssaies de le faire partir.... L’insecte reste là.', 20, 282);
   p.text('C’est comme ça, que tu peux sssssavoir. »', 20, 310);
--- pages/page8/sketch8.js
+++ pages/page8/sketch8.js
@@ -27,6 +27,19 @@ let reX = 420;
 let reC = 420;
 let xoff = 0.0;
 let n;
+
+let insects = [];
+let cnv;
+let s;
+let chance;
+let mvt;
+let angle;
+let turn;
+  
+let minX;
+let maxX;
+let minY;
+let maxY;
   
 p.preload = function() {
  blu = p.loadImage("blu.png");
@@ -43,6 +56,24 @@ p.setup = function() {
  first = p.select("#F");
  second = p.select("#S");
  second.hide();
+
+ chance = 5;
+  mvt = 100;
+  
+  minX = 15;
+  maxX = 1280;
+  minY = 15;
+  maxY = 910;
+   p.angleMode(p.DEGREES);
+
+    //insects
+  for (let i = 0; i < 1; i++) {
+    let x = p.random(950,1000);
+    let y = p.random(325,375);
+    let s = p.random(30,40);
+    insects[i] = new Insect(x, y, s);
+  }
+  
 
   //inter = setInterval(p.switcheroo,1000);
 }
@@ -111,8 +142,20 @@ p.copy(re0,0,0+50*i,blu0.width,50,376+n*i*posneg,179+50*i,blu0.width,50);
 
 if(fade){
   p.background(bck);
+
+  
+  //insects
+  for (let insect of insects) {
+    insect.update();
+    insect.show();
+    insect.checkLimits();
+  }
+
+  
   bck = bck + 0.16;
-  bck = p.constrain(bck,0,255);
+  if (bck> 250){
+second.hide();
+  }
 
   p.textFont("Courier New");
  
@@ -127,10 +170,144 @@ if(fade){
   finOff = finOff + 0.1;
   m = p.noise(finOff) * 10;
 }
+}
+    
     
   
-  
+  class Insect {
+  constructor(x, y, s) {
+    this.pos = p.createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(p.random(1,1.5));
+    this.s = s;
+  }
+
+ checkLimits(){
+   
+   if(this.pos.x < minX){
+      this.vel.x = -this.vel.x
+   }
+   
+   if(this.pos.x > maxX){ 
+       this.vel.x = -this.vel.x
+   }
+   
+   if(this.pos.y < minY){
+     this.vel.y = -this.vel.y
+   }
+   
+   if(this.pos.y > maxY){   
+       this.vel.y = -this.vel.y
+   }
 }
+    
+  update() {
+    this.pos.add(this.vel);  
+  }
+
+  show() {  
+    
+    p.fill(0);
+    p.stroke(0);
+    
+  s = this.s;  
+
+  angle = p.atan(this.vel.y/this.vel.x);
+
+  //translate
+  p.push();
+  p.translate(this.pos.x,this.pos.y);
+    if(this.vel.x<0){
+       p.rotate(angle-90);
+    }else{
+        p.rotate(angle+90);
+    }
+
+ 
+  
+  //body
+  p.beginShape();
+    p.vertex(0, -(s*100/150));
+    p.bezierVertex((s*25/150), -(s*100/150), (s*40/150), -(s*50/150), (s*40/150), -(s*40/150));
+    p.bezierVertex((s*40/150), -(s*15/150), (s*25/150), 0, 0, 0);
+    p.bezierVertex(-(s*25/150), 0, -(s*40/150), -(s*15/150), -(s*40/150), -(s*40/150));
+    p.bezierVertex(-(s*40/150), -(s*50/150), -(s*25/150), -(s*100/150), 0, -(s*100/150));
+    p.endShape();
+  p.bezier(-(s*28/150),-(s*78/150),-(s*10/150),-(s*55/150),(s*10/150),-(s*55/150),(s*28/150),-(s*78/150));
+  
+  
+  //head 
+  p.bezier(-(s*12/150),-(s*97/150),-(s*10/150),-(s*113/150),(s*10/150),-(s*113/150),(s*12/150),-(s*97/150));
+
+  p.bezier(-(s*5/150),-(s*108/150),-(s*7/150),-(s*110/150),-(s*7/150),-(s*110/150),-(s*4/150),-(s*114/150));
+    p.bezier((s*5/150),-(s*108/150),(s*7/150),-(s*110/150),(s*7/150),-(s*110/150),(s*4/150),-(s*114/150));
+   
+  
+
+  //legs
+  p.push();
+p.translate(-(s*17/150),-(s*93/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,-(s*21/150),-(s*16/150));
+p.line(-(s*21/150),-(s*16/150),-(s*28/150),-(s*45/150));
+ p.pop();
+  
+
+  p.push();
+p.translate((s*17/150),-(s*93/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,(s*21/150),-(s*16/150));
+p.line((s*21/150),-(s*16/150),(s*28/150),-(s*45/150));
+ p.pop();
+
+
+
+p.push();
+p.translate(-(s*25/150),-(s*80/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,-(s*25/150),(s*2/150));
+p.line(-(s*25/150),(s*2/150),-(s*37/150),(s*22/150));
+p.pop();
+  
+p.push();
+p.translate((s*25/150),-(s*80/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,(s*25/150),(s*2/150));
+p.line((s*25/150),(s*2/150),(s*37/150),(s*22/150));
+p.pop();
+
+p.push();
+p.translate(-(s*35/150),-(s*60/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,-(s*18/150),(s*18/150));
+p.line(-(s*18/150),(s*18/150),-(s*18/150),(s*45/150));
+p.pop();
+  
+p.push();
+p.translate((s*35/150),-(s*60/150));
+if(p.random(10)<chance){
+ p.rotate(p.random(-(s*mvt/150),(s*mvt/150))); 
+}
+p.line(0,0,(s*18/150),(s*18/150));
+p.line((s*18/150),(s*18/150),(s*18/150),(s*45/150));
+p.pop();
+  
+  p.pop();
+
+  }
+}
+
+
   
 p.switcheroo = function(){
   bool = !bool;
